f540dee9417e0e6d548cd4268e3c1d0bebb4e029
Paul D <44819866+rrep@users.noreply.github.com>
2019-11-13 10:08:41 -0700

    begin creating

--- financial-tracker/js/main.js
+++ financial-tracker/js/main.js
@@ -1,1 +1,12 @@
 // Enter JavaScript for the exercise here...
+const errorLabel = document.querySelector('.error');
+//validate form input (type must be chosen, numeric, positive)
+
+
+//add entered transaction to the transaction table (display $ and two decimal places use .toFixed())
+
+//track total of each type of transaction
+
+//delete transactions (requires a popup)
+
+//timeout that refreshes the page fter 2 minutes
