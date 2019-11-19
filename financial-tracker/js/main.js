22a04b08da12fe077174aa97f20e9dd5e0054d58
pd <44819866+rrep@users.noreply.github.com>
2019-11-19 11:23:02 -0700

    added validation

--- financial-tracker/js/main.js
+++ financial-tracker/js/main.js
@@ -1,9 +1,39 @@
 // Enter JavaScript for the exercise here...
 const errorLabel = document.querySelector('.error');
-//validate form input (type must be chosen, numeric, positive)
 
+document.querySelector('.frm-transactions').addEventListener('submit', function (evt) {
+   //validate form input (description.NotBlank, type = (credit || debit), currency > 0)
+    evt.preventDefault();
+    let descriptionInput = evt.target.elements['description'].value;
+    let typeInput = evt.target.elements['type'].value;
+    let currencyInput = Number.parseFloat(evt.target.elements['currency'].value).toFixed(2);
+    
+    if (!descriptionInput || (typeInput != ("credit" || "debit")) || currencyInput<0){
+        let errorMessages = "Please correct the following errors: <ul>";
+        //Form Validation - Displays proper message for invalid values 
+        if (!descriptionInput){
+            errorMessages += "<li>Please include a description.</li> "
+        }
+        if (typeInput != ("credit" || "debit")){
+            errorMessages += "<li>Please choose either debit or credit.</li> "
+        }
+        if (currencyInput<0){
+            errorMessages += "<li>Please enter a positive amount.</li> "
+        }
+        errorLabel.innerHTML = errorMessages + currencyInput +"</ul>";
+    } else {
 
-//add entered transaction to the transaction table (display $ and two decimal places use .toFixed())
+    var newRow,
+        descriptionCell,
+        amountCell,
+        toolsCell,
+        deleteIcon;
+        
+
+    //add entered transaction to the transaction table (display $ and two decimal places use .toFixed())
+    console.log(descriptionInput)
+    }
+    });
 
 //track total of each type of transaction
 
